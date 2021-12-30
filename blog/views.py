9791474fdf02a7cbf630e578ed764a07c72089a0
rahul-tiwari-95 <knowrahul.95@gmail.com>
2021-12-30 18:53:01 -0500

    made website dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 #we just defined post_list function which we have used in urls.py --> since this is the view --> we need to encode the template
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    allposts = Post.objects.all()
+
+    #this function simply fetches all data of all posts and stores it into 'allposts'
+    return render(request, 'blog/post_list.html', {'posts': allposts})
+
 
 # Create your views here.
+# we used {'posts' : posts} code to refrence it  we have learnt this in Angular and reactJS --> I am assuming that
+# we will be using posts in the html file which we have created inside tempaltes --> blog --> post_list.html
+
